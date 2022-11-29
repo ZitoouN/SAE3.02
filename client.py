e7c13bf9fe33a52d898834cad7f8fcde1cbe427b
ZitoouN <92864882+ZitoouN@users.noreply.github.com>
2022-11-29 01:07:05 +0100

    Update client.py

--- client.py
+++ client.py
@@ -1,31 +1,56 @@
-import socket
+from socket import socket
 import platform
+import threading
+import os
 
-def client_connect():
-    print('Veuillez vous connecter à un serveur')
-    a = input('Entrer une adresse IP : ')
-    p = input('Entrer le port du serveur : ')
-    client_socket = socket.socket()
+def connection():
+    global client
+    print('-----CONNEXION AU SERVEUR-----')
+    a = str(input('Entrer une adresse IP : '))
+    p = int(input('Entrer le port du serveur : '))
+    client = socket()
     try:
-        client_socket.connect((a,p))
+        client.connect((a, p))
     except:
         print('Une erreur a été détecté, veuillez réessayer')
-        return reconnect()
+        return connection()
     else:
         print('La connexion à pu être établie')
 
-def reconnect():
-    adr = input('Adresse IP : ')
-    port = input('Port : ')
-    client_socket = socket.socket()
-    client_socket.connect((adr, port))
 
-
-def communication(client_socket):
+def communication(client):
+    global user
     while True:
-        mess = input(f"{client} -> ")
-        try
-            if mess = 'os':
-                return platform.processor()
-            if mess = 'ram':
-                return 
+        message = input(f"{user}>")
+        client.send(message.encode())
+        if message == "disconnect":
+            client.close()
+            break
+        elif message == "close":
+            print ("Client disconnected")
+            exit()
+
+
+def reception(client):
+    while True:
+        data = client.recv(1024).decode()
+        print(data)
+
+
+def main():
+    global user
+    print('-----CLIENT-----')
+    user = input('Inserer le nom du client : ')
+    connection()
+    t1 = threading.Thread(target=reception, args=[client])
+    t2 = threading.Thread(target=communication, args=[client])
+    t1.start()
+    t2.start()
+    t1.join()
+    t2.join()
+    client.close()
+    print('Successfully disconnected from socket')
+
+
+if __name__ == '__main__':
+   main()
