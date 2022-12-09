15d2dd888f1e529e332d27456b8d6e06b58a5296
ZitoouN <92864882+ZitoouN@users.noreply.github.com>
2022-12-09 20:01:55 +0100

    Create cpu_graph.py

---
+++ cpu_graph.py
@@ -0,0 +1,29 @@
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+from psutil import cpu_percent
+
+frame_len = 200
+y = []
+fig = plt.figure(figsize=(8,4))
+
+def animate(i):
+    y.append(cpu_percent())
+
+    if len(y) <= frame_len:
+        plt.cla()
+        plt.plot(y, 'r')
+    else:
+        plt.cla()
+        plt.plot(y, 'r')
+
+    plt.ylim(0,100)
+    plt.xlabel('Temps en secondes (s)')
+    plt.ylabel('Usage du CPU (%)')
+
+ani = FuncAnimation(plt.gcf(), animate, interval=150)
+
+def graph():
+    plt.show()
+
+if __name__ == '__main__':
+    graph()
